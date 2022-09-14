403e6255aa345038d5c4bb35ce3cd71aba0f8bb1
Shrayansh <shrayansh.tripathi@atmecs.com>
2022-09-14 18:35:06 +0530

    Adding File Book Store Management

---
+++ Book Store Management/pkg/config/app.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "Shrayansh:Atmecs@123/simplerest?")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDb() *gorm.DB {
+	return db
+
+}
